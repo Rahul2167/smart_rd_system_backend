c2e6fd78dcb51a5d928368810486e6f7c1e55aac
Your Name <you@example.com>
2026-02-05 19:24:33 +0530

    Add debug logger to verify active DataSource URL

--- src/main/java/com/rd/project/RdSystemProjectApplication.java
+++ src/main/java/com/rd/project/RdSystemProjectApplication.java
@@ -2,6 +2,7 @@ package com.rd.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
 
 @SpringBootApplication
 public class RdSystemProjectApplication {
@@ -11,4 +12,13 @@ public class RdSystemProjectApplication {
 		System.out.println("success......");
 	}
 
+	@org.springframework.context.annotation.Bean
+	public org.springframework.boot.CommandLineRunner debugDataSource(
+			@org.springframework.beans.factory.annotation.Value("${spring.datasource.url}") String url) {
+		return args -> {
+			System.out.println("==========================================");
+			System.out.println("DEBUG: ACTIVE DATASOURCE URL IS: " + url);
+			System.out.println("==========================================");
+		};
+	}
 }
