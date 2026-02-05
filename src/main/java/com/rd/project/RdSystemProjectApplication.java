932db549087076ce777f296b1f844952abd7ea7b
Your Name <you@example.com>
2026-02-05 17:30:45 +0530

    Add startup log to debug active database URL

--- src/main/java/com/rd/project/RdSystemProjectApplication.java
+++ src/main/java/com/rd/project/RdSystemProjectApplication.java
@@ -6,10 +6,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RdSystemProjectApplication {
 
-	public static void main(String[] args) 
-	{
+	public static void main(String[] args) {
 		SpringApplication.run(RdSystemProjectApplication.class, args);
 		System.out.println("success......");
+	}
+
+	@org.springframework.context.annotation.Bean
+	public org.springframework.boot.CommandLineRunner debugDataSource(
+			@org.springframework.beans.factory.annotation.Value("${spring.datasource.url}") String url) {
+		return args -> {
+			System.out.println("==========================================");
+			System.out.println("DEBUG: ACTIVE DATASOURCE URL IS: " + url);
+			System.out.println("==========================================");
+		};
 	}
 
 }
